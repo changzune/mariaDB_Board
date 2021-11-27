70288df0b197059530ed0df62fe866fc3f964caf
changzune <changzunelee@gmail.com>
2021-11-27 17:51:02 +0900

    #페이징구현

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -88,14 +88,28 @@ public class BoardController {
 		 list = service.list();
 		 model.addAttribute("list", list);   
 		}
+
 		
 		// 게시물 목록 + 페이징 추가
-		@RequestMapping(value = "/listpage", method = RequestMethod.GET)
-		public void getListPage(Model model) throws Exception {
+		@RequestMapping(value = "/listPage", method = RequestMethod.GET)
+		public void getListPage(Model model, @RequestParam("num") int num) throws Exception {
+		 
+		 // 게시물 총 갯수
+		 int count = service.count();
 		  
+		 // 한 페이지에 출력할 게시물 갯수
+		 int postNum = 10;
+		  
+		 // 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
+		 int pageNum = (int)Math.ceil((double)count/postNum);
+		  
+		 // 출력할 게시물
+		 int displayPost = (num - 1) * postNum;
+		    
 		 List list = null; 
-		 list = service.list();
+		 list = service.listPage(displayPost, postNum);
 		 model.addAttribute("list", list);   
+		 model.addAttribute("pageNum", pageNum);
 		}
 		
 		
